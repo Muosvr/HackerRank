d16604575eed1742444fd3acce0864af213548c5
Muosvr <lujason2015@gmail.com>
2018-09-04 18:15:18 -0400

    Cloudy day data structure

---
+++ CloudyDayDataStruct.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+
+        public struct Cloud
+        {
+            public int location, range;
+
+            public Cloud(int loc, int r)
+            {
+                location = loc;
+                range = r;
+            }
+        }
+
+        public struct Town
+        {
+            public int location, population;
+
+            public Town(int loc, int pop)
+            {
+                location = loc;
+                population = pop;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            Cloud cloud1 = new Cloud();
+            Town town1 = new Town();
+
+            cloud1.location = 13;
+            cloud1.range = 2;
+
+            town1.location = 15;
+            town1.population = 300;
+
+            Console.WriteLine("Cloud 1 is at {0} and range {1}", cloud1.location, cloud1.range);
+            Console.WriteLine("Town 1 is at {0} with population of {1}", town1.location, town1.population);
+        
+
+        }
+
+
+
+    }
+}
